f9ebf4d7ce5727a3cd35f45ef1f448476b1e8693
Jorren schepers <s094476@ap.be>
2018-11-29 11:53:35 +0100

    added styling for fitting multiple feeds

--- HTF_IoT-AI/src/app/dashboard/dashboard.component.ts
+++ HTF_IoT-AI/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StreamService } from '../services/stream.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  //styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  private streams : string[]
+  private count : number
+  private width : number
+
+  constructor(private streamSvc : StreamService){}
+
+  ngOnInit(){
+    this.streamSvc.getStreams().subscribe(streams => this.streams = streams.streams);
+    //this.streams = ["camera-1", "camera-1", "camera-1", "camera-1", "camera-1", "camera-1", "camera-1", "camera-1", "camera-1", "camera-1"]
+    this.count = this.streams.length
+    this.width  = 25 - (this.count % 3)
+  }
 }
