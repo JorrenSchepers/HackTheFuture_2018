f592901308764771c5c0ef20ebc9a24a7eb427e0
Jorren schepers <s094476@ap.be>
2018-11-29 09:57:48 +0100

    added dashboardcomponent and routermodule

--- HTF_IoT-AI/src/app/app.module.ts
+++ HTF_IoT-AI/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot([
+      { path: "dashboard", component: DashboardComponent},
+      { path: "", redirectTo:"dashboard", pathMatch: 'full'}
+    ], {useHash: true} )
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
